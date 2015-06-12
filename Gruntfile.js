cc466d9f681481faf27df6636c46eb73341163d0
Jeff Paulo <Jeff.Paulo@mlb.com>
2015-06-12 11:57:14 -0700

    update grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,25 @@
 module.exports = function(grunt){
 	jsFiles = [
-		'public/libs/underscore/underscore.js',
-		'public/libs/jquery/dist/jquery.js',
-		'public/libs/jquery-ui/jquery-ui.js',
-		'public/libs/jquery-ui/ui/core.js',
-		'public/libs/jquery-ui/ui/widget.js',
-		'public/libs/angular/angular.js',
-		'public/libs/angular-animate/angular-animate.js',
-		'public/libs/angular-sanitize/angular-sanitize.js',
-		'public/libs/angular-bootstrap/ui-bootstrap-tpls.js',
-		'public/libs/angular-ui-router/release/angular-ui-router.js',
-		'public/libs/restangular/dist/restangular.js',
-		'public/libs/moment/moment.js',
-		'public/js/modules/nmt-App.js',
-		'public/js/modules/nmt-AppConfig.js',
-		'public/js/modules/nmt-Routing.js',
-		'public/js/controllers/mainController.js',
-		'public/js/controllers/dashboardController.js',
-		'public/js/controllers/welcomeController.js',
-		'public/js/controllers/workoutsController.js',
-		'public/js/controllers/loggerController.js',
-		'public/js/services/**/*.js',
-		'public/js/filters/**/*.js'
+	'public/libs/underscore/underscore.js',
+	'public/libs/jquery/dist/jquery.js',
+	'public/libs/jquery-ui/jquery-ui.js',
+	'public/libs/jquery-ui/ui/core.js',
+	'public/libs/jquery-ui/ui/widget.js',
+	'public/libs/angular/angular.js',
+	'public/libs/angular-animate/angular-animate.js',
+	'public/libs/angular-sanitize/angular-sanitize.js',
+	'public/libs/angular-bootstrap/ui-bootstrap-tpls.js',
+	'public/libs/angular-ui-router/release/angular-ui-router.js',
+	'public/libs/restangular/dist/restangular.js',
+	'public/libs/moment/moment.js',
+	'public/js/modules/nmt-App.js',
+	'public/js/modules/nmt-AppConfig.js',
+	'public/js/modules/nmt-Routing.js',
+	'public/js/controllers/mainController.js',
+	'public/js/controllers/devController.js',
+	'public/js/controllers/searchController.js',
+	'public/js/services/**/*.js',
+	'public/js/filters/**/*.js'
 	],
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
@@ -29,10 +27,10 @@ module.exports = function(grunt){
 			dist: {
 				options: {
 					loadPath: [
-						'public/libs/bootstrap-sass-official/assets/stylesheets/',
-						'public/libs/bootstrap-sass-official/assets/stylesheets/bootstrap/',
-						'public/libs/font-awesome/scss',
-						'public/libs/bootswatch-scss/cyborg/'
+					'public/libs/bootstrap-sass-official/assets/stylesheets/',
+					'public/libs/bootstrap-sass-official/assets/stylesheets/bootstrap/',
+					'public/libs/font-awesome/scss',
+					'public/libs/bootswatch-scss/cyborg/'
 					],
 					style: 'compressed'
 				},
@@ -83,11 +81,11 @@ module.exports = function(grunt){
 			},
 		}
 	});
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-notify');
-	grunt.registerTask('default',['jshint', 'sass', 'concat', 'notify:complete']);
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-notify');
+grunt.registerTask('default',['jshint', 'sass', 'concat', 'notify:complete']);
 }
