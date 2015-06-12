bd64957e7a7efc53aa0417e1fbffacd7fb8d5f3e
Jeff Paulo <Jeff.Paulo@mlb.com>
2015-06-12 11:41:32 -0700

    updated script.js

--- public/builds/script.js
+++ public/builds/script.js
@@ -1,4 +1,4 @@
-/*! nmt 2015-06-11 */
+/*! nmt 2015-06-12 */
 //     Underscore.js 1.8.3
 //     http://underscorejs.org
 //     (c) 2009-2015 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
